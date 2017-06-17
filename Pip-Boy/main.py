d6fcd712eb8ddb354aafa25f75b9d75b80cf9f7f
wizbenkhalifa <youssefbenkhalifa4@gmail.com>
2017-06-17 13:03:22 +0000

    Adattato a rsapberry

--- Pip-Boy/main.py
+++ Pip-Boy/main.py
@@ -3,6 +3,7 @@ Created on 5 Apr 2017
 
 @author: WiZ
 '''
+
 import pygame
 import config
 import os
@@ -14,19 +15,21 @@ from pygame.time import delay
 from interface.Scanlines import Scanlines,Line
 from threading import Lock
 from interface.Settings import Settings
+from interface.Radio import Radio
 
 
 pygame.init()
 screen = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
 done = False
 selected_menu = 0
-menu_list = [home(screen), documents(screen), media(screen), Settings(screen)]
+global menu_list
+menu_list = [home(screen), media(screen), Radio(screen), Map(screen), Settings(screen)]
 if __name__ == '__main__':
     menu_list[selected_menu].renderInterface()
     scanlines = Scanlines(screen)
-    scanlines.run()
     while not done:
         menu_list[selected_menu].renderInterface()
+        scanlines.run()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                     done = True
@@ -61,6 +64,13 @@ if __name__ == '__main__':
                         menu_list[selected_menu].selectionIndex = 2
                 if event.key == pygame.K_RETURN:
                     menu_list[selected_menu].onSelection()
+                if event.key == pygame.K_PLUS:
+                    if selected_menu == 4:
+                        menu_list[selected_menu].onIncrement()
+                if event.key == pygame.K_RETURN:
+                    if selected_menu == 4:
+                        menu_list[selected_menu].onDecrement()
+                        
         pygame.display.flip()
         delay(100)
     
